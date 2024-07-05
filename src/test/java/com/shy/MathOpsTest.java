5be7766633570c72e250f1c07329053df4626183
Shylaja Nagineni <Shylajanagineni.jobs@gmail.com>
2024-07-04 21:13:29 -0700

    assert test method

--- src/test/java/com/shy/MathOpsTest.java
+++ src/test/java/com/shy/MathOpsTest.java
@@ -1,16 +1,32 @@
 package com.shy;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.*;
 
 public class MathOpsTest {
+
+    @Test
+    public void simpleAdd(){
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void isNumFound(){
+        List<Integer> i = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        int targetnum = 9;
+        Assert.assertTrue(i.contains(targetnum));
+    }
+
     @Test
     public void getUnique() {
-        List<String> li = Arrays.asList("one", "one", "two");
+        List<String> li = Arrays.asList(" one", " one ", "two ");
         Set<String> s = new HashSet<>();
 
         for (String i : li) {
+            i = i.trim().toUpperCase();
             s.add(i);
         }
         System.out.println(s);
